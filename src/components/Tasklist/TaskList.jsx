4a22ff6bfbbec44f91474d3d263bc0a2e830d550
Sameer <smr.naeemdar@gmail.com>
2024-10-23 20:50:42 +0500

    change in tasklistNumber file for dynamic get data fron local stroge add[0]

--- src/components/Tasklist/TaskList.jsx
+++ src/components/Tasklist/TaskList.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 
-const TaskList = () => {
+const TaskList = ({data}) => {
   return (
     <div  id='tasklist' className='h-[55%] overflow-x-auto flex items-center justify-start gap-5 flex-nowrap w-full py-5 mt-10' >
         <div className=' flex-shrink-0 h-full w-[300px] p-5 bg-green-400 rounded-xl '>
--- src/components/other/TaskListNumbers.jsx
+++ src/components/other/TaskListNumbers.jsx
@@ -1,55 +1,31 @@
-import React from 'react'
+import React from "react";
+
 
 const TaskListNumbers = ({data}) => {
   return (
-    <div className='flex mt-10 screen justify-between gap-5'>
-        <div className=' rounded-xl w-[45%] py-6 px-9 bg-red-400 '  >
-            <h2 className='text-2xl font-semibold'>
-                {data.tasks.newTask}
+    <div className="flex mt-10 screen justify-between gap-5">
+      <div className=" rounded-xl w-[45%] py-6 px-9 bg-red-400 ">
+        <h2 className="text-2xl font-semibold">{data.tasks[0].newTask}</h2>
+        <h3 className="text-xl font-medium ">New Task</h3>
+      </div>
 
-            </h2>
-            <h3 className='text-xl font-medium ' >
-                New Task
+      <div className=" rounded-xl w-[45%] py-6 px-9 bg-blue-400 ">
+        <h2 className="text-2xl font-semibold">{data.tasks[0].active}</h2>
+        <h3 className="text-xl font-medium">Completed Task</h3>
+      </div>
 
-            </h3>
-        </div>
-
-        <div className=' rounded-xl w-[45%] py-6 px-9 bg-blue-400 '  >
-            <h2 className='text-2xl font-semibold'>
-            {data.tasks.newTask}
-
-            </h2>
-            <h3 className='text-xl font-medium' >
-                Completed Task
-
-            </h3>
-        </div>
-
-        {/* 3rdCard */}
-        <div className=' rounded-xl w-[45%] py-6 px-9 bg-green-400 '  >
-            <h2 className='text-2xl font-semibold'>
-            {data.tasks.completed}
-
-            </h2>
-            <h3 className='text-xl font-medium' >
-                Accepted Task
-
-            </h3>
-        </div>
-        {/* 4th-Card */}
-        <div className=' rounded-xl w-[45%] py-6 px-9 bg-purple-400 '  >
-            <h2 className='text-2xl font-semibold'>
-            {data.tasks.failed}
-
-            </h2>
-            <h3 className='text-xl font-medium' >
-                Failed Task
-
-            </h3>
-        </div>
-
+      {/* 3rdCard */}
+      <div className=" rounded-xl w-[45%] py-6 px-9 bg-green-400 ">
+        <h2 className="text-2xl font-semibold">{data.tasks[0].completed}</h2>
+        <h3 className="text-xl font-medium">Accepted Task</h3>
+      </div>
+      {/* 4th-Card */}
+      <div className=" rounded-xl w-[45%] py-6 px-9 bg-purple-400 ">
+        <h2 className="text-2xl font-semibold">{data.tasks[0].failed}</h2>
+        <h3 className="text-xl font-medium">Failed Task</h3>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default TaskListNumbers
+export default TaskListNumbers;
