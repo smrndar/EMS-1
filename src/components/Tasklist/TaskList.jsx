a4a7c0c1e4b238d4f0740b2faa77856b67107478
Sameer <smr.naeemdar@gmail.com>
2024-10-23 17:10:38 +0500

    Make a ine more task with High Card

--- src/components/Tasklist/TaskList.jsx
+++ src/components/Tasklist/TaskList.jsx
@@ -65,6 +65,19 @@ const TaskList = () => {
 
         </div>
        
+        <div className=' flex-shrink-0 h-full w-[300px] p-5 bg-yellow-400 rounded-xl '>
+         <div className='flex justify-between items-center' > 
+        <h2 className='bg-red-600 text-sm px-3 py-1 rounded ' >High</h2>
+        <h4 className='text-sm'>22-OCt-2024</h4>
+
+        </div>
+        <h2 className='mt-5 text-2xl font-semibold' >Make a youtube video</h2>
+        <p className='text-sm  mt-2'>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Autem corrupti iusto recusandae voluptate debitis ullam.
+        </p>
+
+        </div>
+       
     </div>
   )
 }
